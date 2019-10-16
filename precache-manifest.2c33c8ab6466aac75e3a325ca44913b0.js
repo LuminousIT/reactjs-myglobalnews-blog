8af8c0a39bd38a7dd978e113a16e376b0a5209c1
Olumide Samuel <oluwoleit@gmail.com>
2019-10-16 15:56:03 +0100

    Updates

---
+++ precache-manifest.2c33c8ab6466aac75e3a325ca44913b0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d3e4c4fcc0fd3dc939b56a056747fe",
+    "url": "/reactjs-myglobalnews-blog/index.html"
+  },
+  {
+    "revision": "2e42c029136646193557",
+    "url": "/reactjs-myglobalnews-blog/static/js/2.57e01719.chunk.js"
+  },
+  {
+    "revision": "35dacc47c03824e29e6a",
+    "url": "/reactjs-myglobalnews-blog/static/js/main.464ea1d3.chunk.js"
+  },
+  {
+    "revision": "23597bf47a959ab858bb",
+    "url": "/reactjs-myglobalnews-blog/static/js/runtime~main.6795ea57.js"
+  }
+]);
